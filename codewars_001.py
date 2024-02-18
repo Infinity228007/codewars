a78ba9ba63dcde8519055c55e438dbf8b2ac6c6a
Porozov Ivan <139009015+Infinity228007@users.noreply.github.com>
2024-02-18 19:18:23 +0200

    Update codewars_001.py
    Methods were added

--- codewars_001.py
+++ codewars_001.py
@@ -1,13 +1,53 @@
-def narcissistic(value):
+def narcissistic(value: int):
     digits = [int(num) for num in str(value)]
     n = len(digits)
-    if sum([num**n for num in digits]) == value:
+    if len(str(value)) == 1:
+        return True
+    elif sum([num ** n for num in digits]) == value:
         return True
     else:
-        return False  # Code away
+        return False
 
 
-narcissistic(7)
-narcissistic(371)
-narcissistic(122)
-narcissistic(4887)
+print(narcissistic(7))
+print(narcissistic(371))
+print(narcissistic(122))
+print(narcissistic(4887))
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+def dirReduc(arr):
+    opposites = {"NORTH": "SOUTH", "SOUTH": "NORTH", "EAST": "WEST", "WEST": "EAST"}
+    stack = []
+    for direction in arr:
+        if stack and stack[-1] == opposites[direction]:
+            stack.pop()
+        else:
+            stack.append(direction)
+    return stack
+
+
+print(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]))  # Output: ['WEST']
+print(dirReduc(["NORTH", "SOUTH", "EAST", "WEST"]))  # Output: []
+print(dirReduc(["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"]))  # Output: ['WEST', 'WEST']
+
+
+def tower_builder(n_floors):
+    tower = []
+    for i in range(n_floors):
+        spaces = " " * (n_floors - i - 1)
+        stars = "*" * (2 * i + 1)
+        tower.append(spaces + stars + spaces)
+    return tower
+
+
+print(tower_builder(3))
+print(tower_builder(6))
